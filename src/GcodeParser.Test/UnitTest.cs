620895e44c3de8e827b8f91be6b63867e29ca59d
Andreas <andy.reitberger@gmail.com>
2023-07-18 20:11:10 +0200

    Sum `Volume` for multi extruders on `OrcaSlicer`
    Fixed #28

--- src/GcodeParser.Test/UnitTest.cs
+++ src/GcodeParser.Test/UnitTest.cs
@@ -1,18 +1,13 @@
-﻿using AndreasReitberger.Parser.Gcode;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Threading;
-using System.Threading.Tasks;
+using AndreasReitberger.Parser.Gcode;
+using Xunit.Sdk;
 
-namespace GcodeParserSharp.Test
+namespace GcodeSharpParser.Test
 {
-    [TestClass]
-    public class UnitTest
+    public class UnitTest1
     {
         public List<string> testfiles = new List<string>()
         {
+            @"C:\Users\Andreas\Desktop\Gcodes\BKG_ESP32_V4_ASA_25m48s.gcode",
             @"Gcodes\BambuStudio.gcode",
             @"Gcodes\PrusaSlicer2.gcode",
             @"Gcodes\OrcaSlicer.gcode",
@@ -23,7 +18,7 @@ namespace GcodeParserSharp.Test
             @"Gcodes\Simplify3D.gcode",
         };
 
-        [TestMethod]
+        [Fact]
         public async Task ParseGcodesAsync()
         {
             var prog = new Progress<int>(percent =>
@@ -46,7 +41,7 @@ namespace GcodeParserSharp.Test
                     {
                         Console.WriteLine($"Gcode parsed in: {gcode.ParsingDuration}");
                         // Make sure that print time and volume is not 0 
-                        Assert.IsTrue(gcode.PrintTime > 0 && gcode.ExtrudedFilamentVolume > 0);
+                        Assert.True(gcode.PrintTime > 0 && gcode.ExtrudedFilamentVolume > 0);
                     }
                     else
                         Assert.Fail($"Parsed gcoe was null: {file}");
